24136153fb02031cc5fd7f7f08ca98f1a7f386dc
anipedia <56411317+anipedia@users.noreply.github.com>
2020-02-20 12:21:19 -0800

    Update index.js

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ firebase.auth().onAuthStateChanged(function(user) {
 	  
       document.getElementById("user_para").innerHTML = email_id;
       document.getElementById("auser_para").innerHTML = name;
-      document.getElementById("photo_para").innerHTML = photoUrl;
+      document.getElementById("photo_para").src= photoUrl;
 
     }
 
