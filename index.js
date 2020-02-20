c49ce7f015f7f1bd7e7ff66d24d5862a280ac2c7
anipedia <56411317+anipedia@users.noreply.github.com>
2020-02-20 03:20:00 -0800

    Update index.js

--- index.js
+++ index.js
@@ -30,7 +30,6 @@ firebase.auth().onAuthStateChanged(function(user) {
     // User is signed in.
 
     document.getElementById("user_div").style.display = "block";
-    document.getElementById("auser_div").style.display = "block";
     document.getElementById("login_div").style.display = "none";
 
     var user = firebase.auth().currentUser;
@@ -46,7 +45,6 @@ firebase.auth().onAuthStateChanged(function(user) {
     // No user is signed in.
 
     document.getElementById("user_div").style.display = "none";
-    document.getElementById("auser_div").style.display = "none";
     document.getElementById("login_div").style.display = "block";
 
   }
