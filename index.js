b9fb052825d75a397fb5912d0249e9be22880786
Anipedia <56411317+anipedia@users.noreply.github.com>
2020-02-20 22:11:14 -0800

    Update index.js

--- index.js
+++ index.js
@@ -35,6 +35,32 @@ var firebaseConfig = {
   var credential = error.credential;
 });
  }
+
+function Github(){
+  var provider = new firebase.auth.GithubAuthProvider();
+  firebase.auth().signInWithRedirect(provider).then(function(result) {
+  var token = result.credential.accessToken;
+  var user = result.user;
+}).catch(function(error) {
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  var email = error.email;
+  var credential = error.credential;
+});
+ }
+
+function Microsoft(){
+  var provider = new firebase.auth.MicrosoftAuthProvider();
+  firebase.auth().signInWithRedirect(provider).then(function(result) {
+  var token = result.credential.accessToken;
+  var user = result.user;
+}).catch(function(error) {
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  var email = error.email;
+  var credential = error.credential;
+});
+ }
 	
 firebase.auth().onAuthStateChanged(function(user) {
   if (user) {
