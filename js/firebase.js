1878d6b9e0521ed533366e2c32a645d1488184eb
Anipedia <56411317+anipedia@users.noreply.github.com>
2020-02-21 04:58:39 -0800

    Create firebase.js

---
+++ js/firebase.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAYGHW1fppyfoW7AnNufPnEm4thtCM6KfE",
+    authDomain: "login-a7858.firebaseapp.com",
+    databaseURL: "https://login-a7858.firebaseio.com",
+    projectId: "login-a7858",
+    storageBucket: "login-a7858.appspot.com",
+    messagingSenderId: "596338404902",
+    appId: "1:596338404902:web:03298431a4afc8a5b240e7",
+    measurementId: "G-1G0PSD3LND"
+  };
+  firebase.initializeApp(firebaseConfig);
