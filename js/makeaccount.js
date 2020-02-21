b5d8833ee32d1dd62c7b655b2786978fa1473b7e
Anipedia <56411317+anipedia@users.noreply.github.com>
2020-02-21 06:21:51 -0800

    Update makeaccount.js

--- js/makeaccount.js
+++ js/makeaccount.js
@@ -2,19 +2,23 @@ function createAccount() {
     var name = document.getElementById('user_name').value;
     var em = document.getElementById('user_email').value;
     var pass = document.getElementById('user_pass').value;
-    var imageUrl = document.getElementById('user_pic').value;
     
     firebase.auth().createUserWithEmailAndPassword(em, pass)
     
     .then(function(success) {
-       function writeUserData(userId, name, email, imageUrl) {
-  firebase.database().ref('users/' + userId).set({
-    username: name,
-    email: em,
-    password: pass,
-    profile_picture : imageUrl
-  });
-}
+        var userId =firebase.database().ref('Users/').push();
+
+        var userObj = {
+            id : userId,
+            uname : name,
+            email : em,
+            password : pass
+        }
+
+        skey.set(userObj).then(function(success) {
+            alert("Your account has been created.");
+            location.href='index.html';
+        });
 
     })
     
