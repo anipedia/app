5c56bb8155e501195daf150b17be667dbc6ff4d7
Anipedia <56411317+anipedia@users.noreply.github.com>
2020-03-05 21:06:42 +0500

    Update amv.js

--- upload/amv.js
+++ upload/amv.js
@@ -1,1 +1,27 @@
 
+var myArray = [
+{
+"display": "JavaScript 4444Tutorial",
+"mark": "fiber_new",
+"markcolor": "color-white",
+"":"",
+"url": "https://www.w3schools.com/js/default.asp",
+"video": "/vangod%20on%20Instagram_%20_ditf%20edit__Bw4YtKFgQ3_(MP4).mp4?raw=true#t=0.01",
+"download": "/vangod%20on%20Instagram_%20_ditf%20edit__Bw4YtKFgQ3_(MP4).mp4?raw=true"
+},{
+"display": "JavaScript 3333Tutorial",
+"mark": "fiber_new",
+"markcolor": "color-black",
+"url": "https://www.w3schools.com/js/default.asp",
+"video": "/vangod%20on%20Instagram_%20_ditf%20edit__Bw4YtKFgQ3_(MP4).mp4?raw=true#t=0.01",
+"download": "/vangod%20on%20Instagram_%20_ditf%20edit__Bw4YtKFgQ3_(MP4).mp4?raw=true"
+},{
+"display": "JavaScript 22Tutorial",
+"url": "https://www.w3schools.com/js/default.asp",
+"video": "/vangod%20on%20Instagram_%20_ditf%20edit__Bw4YtKFgQ3_(MP4).mp4?raw=true#t=0.01"
+},{
+"display": "JavaScript 111Tutorial",
+"url": "https://www.w3schools.com/js/default.asp",
+"video": "/vangod%20on%20Instagram_%20_ditf%20edit__Bw4YtKFgQ3_(MP4).mp4?raw=true#t=0.01"
+}
+];
