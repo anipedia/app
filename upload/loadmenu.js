b88c88110ab5edf1497c4c0789ea3de051fb097f
Anipedia <56411317+anipedia@users.noreply.github.com>
2020-03-06 14:44:53 +0500

    Create loadmenu.js

---
+++ upload/loadmenu.js
@@ -0,0 +1,12 @@
+var myArray = [
+{
+ "iname" : "Anipedoa",
+ "iurl" : "https://anipedia.github.io/app/redM.png"	
+},{
+ "iname" : "Aniped11",
+ "iurl" : "https://anipedia.github.io/app/a.png"	
+},{
+ "iname" : "Aniped33",
+ "iurl" : "https://anipedia.github.io/app/AA.png"	
+}
+];
