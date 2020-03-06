9cb7bd90100369f31ae658d7a7ff791b17f615fe
Anipedia <56411317+anipedia@users.noreply.github.com>
2020-03-06 17:46:40 +0500

    Update loadmenu.js

--- upload/loadmenu.js
+++ upload/loadmenu.js
@@ -1,12 +1,12 @@
 var myArray = [
 }
  "iname" : "One Piece",
- "iurl" : "https://animekisa.tv/img/coversjpg/one-piece.jpg.webp"
+ "iurl" : "https://animekisa.tv/img/coversjpg/one-piece.jpg"
 },{
  "iname" : "One Piece",
- "iurl" : "https://animekisa.tv/img/coversjpg/one-piece.jpg.webp"
+ "iurl" : "https://animekisa.tv/img/coversjpg/one-piece.jpg"
 },{
  "iname" : "One Piece",
- "iurl" : "https://animekisa.tv/img/coversjpg/one-piece.jpg.webp"
+ "iurl" : "https://animekisa.tv/img/coversjpg/one-piece.jpg"
 }
 ];
