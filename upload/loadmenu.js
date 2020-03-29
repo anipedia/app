2fffe173708b8ac2755c8e0f88fa948d45714302
Anipedia <56411317+anipedia@users.noreply.github.com>
2020-03-29 06:03:24 -0700

    Update loadmenu.js

--- upload/loadmenu.js
+++ upload/loadmenu.js
@@ -1,12 +1,37 @@
-var myArray = [
-}
- "iname" : "One Piece",
- "iurl" : "https://animekisa.tv/img/coversjpg/one-piece.jpg"
+[
+{ 
+
+ "iname" : "Hero <br> Academia Season 4", 
+ "iurl" : "boku-no-hero-academia-4",
+ "ilink" : "Google.com",
+ "itext" : "",
+ "itextstyle" : "margin-top: -52px;z-index: 2; position:relative; line-height: 0.4cm;"
+},{ 
+
+ "iname" : "Sword Art Online", 
+ "iurl" : "sword-art-online",
+ "ilink" : "fast.com",
+ "itext" : "",
+ "itextstyle" : "margin-top: -37px;z-index: 2; position:relative; line-height: 0.4cm;"
 },{
- "iname" : "One Piece",
- "iurl" : "https://animekisa.tv/img/coversjpg/one-piece.jpg"
+
+ "iname" : "Black <br> Clover", 
+ "iurl" : "black-clover",
+ "ilink" : "fast.com",
+ "itext" : "",
+ "itextstyle" : "margin-top: -37px;z-index: 2; position:relative; line-height: 0.4cm;"
+},{ 
+
+ "iname" : "Naruto: Shippuden", 
+ "iurl" : "naruto-shippuden",
+ "ilink" : "fast.com",
+ "itext" : "",
+ "itextstyle" : "margin-top: -37px;z-index: 2; position:relative; line-height: 0.4cm;"
 },{
- "iname" : "One Piece",
- "iurl" : "https://animekisa.tv/img/coversjpg/one-piece.jpg"
-}
-];
+
+ "iname" : "One<br>Piece", 
+ "iurl" : "one-piece",
+ "ilink" : "",
+ "itext" : "",
+ "itextstyle" : "margin-top: -34px;z-index: 2; position:relative; line-height: 0.4cm;"
+}]
